dbc3b8578d2903e034e8b86ae91dbbecd520db4a
chaddotson <chad.dotson@gmail.com>
2015-12-25 11:06:37 -0600

    Create bounce_mDNSResponder.sh

---
+++ bounce_mDNSResponder.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# http://apple.stackexchange.com/questions/26616/dns-not-resolving-on-mac-os
+
+launchctl unload -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
+launchctl load -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
